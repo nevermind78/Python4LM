8ef247c1da3ad56b04e37bb70b5b5f237a5a0de0
Abdallah Khemais <45246688+nevermind78@users.noreply.github.com>
2024-02-18 23:40:46 +0100

    Update code.py

--- TP12024/code.py
+++ TP12024/code.py
@@ -1,1 +1,15 @@
+def identite(s : str) -> str :
+    """ Renvoie la chaine s telle quelle """
+    return s
 
+assert identite("LU1IN011") == "LU1IN011"
+assert identite("") == ""
+
+def identite_texte(nom : str) -> None :
+    """ Precondition : <nom>.txt est un fichier existant
+    recopie le contenu du fichier <nom>.txt dans <nom>-copie.txt """
+    with open(nom + ".txt", "r") as source :
+        with open(nom + "-copie.txt", "w") as destination :
+            ligne : str
+            for ligne in source.readlines() :
+                destination.write(identite(ligne))
